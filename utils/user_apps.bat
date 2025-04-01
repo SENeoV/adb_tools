4351850c6f1910cf4bbec0946a74597537139317
SENeoV <seneouve@gmail.com>
2025-04-02 00:20:22 +0200

    -

--- utils/user_apps.bat
+++ utils/user_apps.bat
@@ -12,11 +12,3 @@ adb shell pm list packages -3 | find /c "package:"
 echo -------------------------------
 pause
 exit /b
-
-:adb_error
-echo Error: No device connected or unauthorized
-echo 1. Verify USB connection
-echo 2. Enable USB Debugging
-echo 3. Allow USB debugging prompt
-pause
-exit /b 1
