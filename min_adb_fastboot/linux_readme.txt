44c8d8a94c3cac1bc47cc6007803f5e19aa21981
SENeoV <seneouve@gmail.com>
2025-04-02 00:28:24 +0200

    FIX: - check_adb.bat improved. FEAT: - Added linux_readme.txt

---
+++ min_adb_fastboot/linux_readme.txt
@@ -0,0 +1,9 @@
+-- Debian/Ubuntu/L.Mint --
+sudo apt install android-tools-adb
+sudo apt install android-tools-fastboot
+
+-- Fedora/openSUSE --
+sudo dnf install android-tools
+
+-- ArchLinux --
+sudo pacman -Sy android-tools
--- utils/common/check_adb.bat
+++ utils/common/check_adb.bat
@@ -1,16 +1,24 @@
 @echo off
+:: Script to verify ADB connectivity
 
-:: Verify ADB connectivity
+:: Navigate to the min_adb_fastboot directory
 cd /d "%~dp0..\..\min_adb_fastboot"
+
+:: Check for connected ADB devices
 adb devices | findstr /r /c:"device$" >nul
-if %errorlevel% neq 0 goto :adb_error
+if %errorlevel% neq 0 (
+    call :adb_error
+    exit /b 1
+)
+:: Exit successfully if a device is connected
 exit /b 0
 
-:: Error debug
+:: Error handling for ADB connectivity issues
 :adb_error
-echo Error: No device connected or unauthorized
-echo 1. Verify USB connection
-echo 2. Enable USB Debugging
-echo 3. Allow USB debugging prompt
+echo =============================================================
+echo Error: No device connected or unauthorized. Please check the following:
+echo 1. Verify that the USB connection is properly established.
+echo 2. Ensure USB Debugging is enabled on your device.
+echo 3. Allow the USB debugging prompt on your device.
+echo =============================================================
 pause
-exit /b 1
